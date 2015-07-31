b14271a89120a4515009b64bcd6cdc46ce7eab75
danielGRH <dnlmartinezfernandez@gmail.com>
2015-07-30 22:15:45 -0500

    Test

--- contact/contact.php
+++ contact/contact.php
@@ -23,8 +23,9 @@ $error = '';
 
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-$headers .= "From: ".$name." <".$email."> \r\n";
-$headers .= "Reply-To: ".WEBMASTER_EMAIL."\r\n";
+
+// More headers
+$headers .= 'From: <'.$email.'>' . "\r\n";
 
 // $message = (!empty($message)) ? $message : null;
 // $name = (!empty($name)) ? $name : null;
