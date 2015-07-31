d5a1b9638c84793356def7e1a08b4e3ed69b29f7
danielGRH <dnlmartinezfernandez@gmail.com>
2015-07-30 22:23:39 -0500

    Test

--- contact/contact.php
+++ contact/contact.php
@@ -19,12 +19,8 @@ $subject = stripslashes($_POST['subject']);
 $message = stripslashes($_POST['message']);
 
 
-$error = '';
-
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// More headers
 $headers .= 'From: <'.$email.'>' . "\r\n";
 
 // $message = (!empty($message)) ? $message : null;
@@ -32,31 +28,30 @@ $headers .= 'From: <'.$email.'>' . "\r\n";
 // $subject = (!empty($subject)) ? $subject : null;
 // $email = (!empty($email)) ? $email : "ubuntu@oquetza.mx"; 
 
-$html = "<h1>Hola</h1>";
-// $html = ' <table style="border: 1px solid #ccc; width: 50%;">
-// 						<tr style="background: #f9f9f9;">
-// 							<td><img src="http://www.oquetza.mx/img/logo.png" width="50px" style="padding: 10px;" /> <span style="position: absolute; top: 40px; left: 80px; font-family: Open Sans, Arial, sans-serif; color: #777; display:inline-block;"><b style="color: #377AC0;">C</b>OLEGIO <b style="color: #377AC0;">O</b>QUETZA</span></td>
-// 						</tr>
-// 						<tr>
-// 							<td>
-// 								<div style="padding: 10px 10px 10px 15px; font-family: Open Sans, Arial, sans-serif;">
-// 									<h4>Informaci&oacute;n del cliente: </h4>
-// 									<p><b>Nombre:</b> '.$name.'</p>
-// 									<p><b>Mail:</b> '.$email.'</p>
-// 									<p>'.$message.'</p>
-// 								</div>
-// 							</td>
-// 						</tr>
-// 						<tr>
-// 							<td>
-// 								<div style="width: 100%; height: 50px; background: #377AC0;"></div>
-// 							</td>
-// 						</tr>
-// 					</table>';
+$html = '<html><body> <table style="border: 1px solid #ccc; width: 50%;">
+						<tr style="background: #f9f9f9;">
+							<td><img src="http://www.oquetza.mx/img/logo.png" width="50px" style="padding: 10px;" /> <span style="position: absolute; top: 40px; left: 80px; color: #777; display:inline-block;"><b style="color: #377AC0;">C</b>OLEGIO <b style="color: #377AC0;">O</b>QUETZA</span></td>
+						</tr>
+						<tr>
+							<td>
+								<div style="padding: 10px 10px 10px 15px;">
+									<h4>Informaci&oacute;n del cliente: </h4>
+									<p><b>Nombre:</b> '.$name.'</p>
+									<p><b>Mail:</b> '.$email.'</p>
+									<p>'.$message.'</p>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<div style="width: 100%; height: 50px; background: #377AC0;"></div>
+							</td>
+						</tr>
+					</table></body></html>';
 
 if(!$error)
 {
-$mail = mail(WEBMASTER_EMAIL, $subject, $html, $headers);
+	$mail = mail(WEBMASTER_EMAIL, $subject, $html, $headers);
 
 
 if($mail)
